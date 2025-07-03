0b3b1781a555b0b8259edfc8df3c24a4e72d647a
Magazet <2820685440@qq.com>
2025-07-03 14:19:13 +0800

    fix: 修复括号不匹配问题

--- UsrAI.cpp
+++ UsrAI.cpp
@@ -1,7 +1,7 @@
 #include "UsrAI.h"
-#include<set>
+#include <set>
 #include <iostream>
-#include<unordered_map>
+#include <unordered_map>
 using namespace std;
 tagGame tagUsrGame;
 ins UsrIns;
@@ -13,11 +13,18 @@ tagInfo info;
 void UsrAI::processData()
 {
     info = getInfo();
+
     for (tagBuilding& building : info.buildings) {
-    //如果市镇中心空闲，且有足够的食物，且农民数量小于人口上限，则生产农民
+
+    // 如果市镇中心空闲，且有足够的食物，且农民数量小于人口上限，则生产农民
     if (building.Type == BUILDING_CENTER && building.Project == 0 &&
         info.Meat >= BUILDING_CENTER_CREATEFARMER_FOOD &&
-        info.farmers.size() < info.Human_MaxNum) {
+        info.farmers.size() < info.Human_MaxNum * 0.75) {
         BuildingAction(building.SN, BUILDING_CENTER_CREATEFARMER);
         break;
+       }
+    }
+
+
 }
+
